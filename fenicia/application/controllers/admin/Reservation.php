9dadf3592e08454b30199f91d5f8b48740d662ce
metgituser <soumo.pratihar@fitser.com>
2020-11-02 18:30:40 +0530

    sms 3

--- fenicia/application/controllers/admin/Reservation.php
+++ fenicia/application/controllers/admin/Reservation.php
@@ -251,10 +251,13 @@ class Reservation extends MY_Controller {
 				}
 				if(!empty($mobile)){
 					/**  modified by ishani on 09.09.2020 **/
-					$message  = "Thank you for confirming your Reservation at Club Fenicia. Your reservation details are here: \n";
-                  	$message .= "Booking ref no.".$reservation_id."\n Date: ".$mail['reservation_date']."\n Time: ".$mail['reservation_time']."\n Time: "."\n No. of Guests: ".$mail['no_of_guest']."\n Status: Confirmed." ;
-                  	$message .= "WE WOULD BE HOLDING YOUR RESERVATION FOR 15 MINUTES FROM THE TIME OF RESERVATION AND IT WILL BE RELEASED WITHOUT ANY PRIOR INFORMATION.";
-                  	$this->smsSend($mobile,$message);
+					if($reservation_id!='' && $this->input->post("reservation_date")!='' && $this->input->post("reservation_time")!='')
+					{
+						$message  = "Thank you for confirming your Reservation at Club Fenicia. Your reservation details are here: \n";
+	                  	$message .= "Booking ref no.".$reservation_id."\n Date: ".$this->input->post("reservation_date")."\n Time: ".$this->input->post("reservation_time")."\n No. of Guests: ".$this->input->post( 'no_of_guests' )."\n Status: Confirmed." ;
+	                  	$message .= "WE WOULD BE HOLDING YOUR RESERVATION FOR 15 MINUTES FROM THE TIME OF RESERVATION AND IT WILL BE RELEASED WITHOUT ANY PRIOR INFORMATION.";
+	                  	$this->smsSend($mobile,$message);
+	                }
                   	///////////////////////////////////////////////////////////////
 				}
               	$this->session->set_flashdata('error_msg','');
@@ -469,11 +472,14 @@ class Reservation extends MY_Controller {
 				}
 				if(!empty($mobile)){
 					/** modified by ishani on 09.09.2020 **/
-					$reservation_id=$result ;
-					$message  = "Thank you for confirming your Reservation at Club Fenicia. Your reservation details are here: \n";
-                  	$message .= "Booking ref no.".$reservation_id."\n Date: ".$this->input->post("reservation_date")."\n Time: ".$this->input->post("reservation_time")."\n No. of Guests: ".$this->input->post( 'no_of_guests' )."\n Status: Confirmed. \n";
-                  	$message .= "WE WOULD BE HOLDING YOUR RESERVATION FOR 15 MINUTES FROM THE TIME OF RESERVATION AND IT WILL BE RELEASED WITHOUT ANY PRIOR INFORMATION.";
-                  	$this->smsSend($mobile,$message);
+					if($reservation_id!='' && $this->input->post("reservation_date")!='' && $this->input->post("reservation_time")!='')
+					{
+						$reservation_id=$result ;
+						$message  = "Thank you for confirming your Reservation at Club Fenicia. Your reservation details are here: \n";
+	                  	$message .= "Booking ref no.".$reservation_id."\n Date: ".$this->input->post("reservation_date")."\n Time: ".$this->input->post("reservation_time")."\n No. of Guests: ".$this->input->post( 'no_of_guests' )."\n Status: Confirmed. \n";
+	                  	$message .= "WE WOULD BE HOLDING YOUR RESERVATION FOR 15 MINUTES FROM THE TIME OF RESERVATION AND IT WILL BE RELEASED WITHOUT ANY PRIOR INFORMATION.";
+	                  	$this->smsSend($mobile,$message);
+	                }
 
                   	//echo $message; die;
 
